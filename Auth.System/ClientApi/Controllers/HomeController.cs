ab329c841eff17ba56d4fb37df84c6195a2694cc
UpDown12 <45355822+UpDown12@users.noreply.github.com>
2020-09-13 12:50:08 +0300

    Added connection user and api

---
+++ Auth.System/ClientApi/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ClientApi.Models;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ClientApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public HomeController(ILogger<HomeController> logger, IHttpClientFactory httpClientFactory)
+        {
+            _logger = logger;
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> GetSecret()
+        {
+            var client = httpClientFactory.CreateClient();
+
+            var response = await client.GetAsync("https://localhost:5001/Home/Secret");
+
+            var message = await response.Content.ReadAsStringAsync();
+
+            ViewData["Message"] = message;
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Auth.System/IdentityServer/Config.cs
@@ -0,0 +1,31 @@
+﻿using IdentityModel;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<Client> GetClients() =>
+            new List<Client>
+            {
+                new Client()
+                {
+                    ClientId = "ClientMvc",
+                    ClientSecrets = {new Secret("ClientSecret".ToSha256()) },
+                    AllowedGrantTypes = GrantTypes.Code,
+                    AllowedScopes = 
+                    {
+                        "ClientMvc"
+                    }
+
+                }
+            };
+        public static IEnumerable<IdentityResource> GetIdentityResources() =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId()
+            };
+
+    }
+}
